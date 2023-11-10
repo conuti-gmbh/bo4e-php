52de80d58e89b391c58b2f951f19b091fb8f7f6a
Jenkins Auto Sync Pipeline <jenkins@conuti.de>
2023-11-10 10:53:12 +0000

    Auto Sync at Fri Nov 10 10:53:07 UTC 2023

--- src/v1/COM/Abweichung.php
+++ src/v1/COM/Abweichung.php
@@ -14,6 +14,16 @@ class Abweichung
         readonly ?string $zugehoerigeRechnung,
         readonly ?string $abweichungsgrundCode,
         readonly ?string $abweichungsgrundCodeliste,
+        readonly ?string $fehlendePositionen1,
+        readonly ?string $fehlendePositionen2,
+        readonly ?string $fehlendePositionen3,
+        readonly ?string $fehlendePositionen4,
+        readonly ?string $fehlendePositionen5,
+        readonly ?string $abweichungsgrundBemerkung1,
+        readonly ?string $abweichungsgrundBemerkung2,
+        readonly ?string $abweichungsgrundBemerkung3,
+        readonly ?string $abweichungsgrundBemerkung4,
+        readonly ?string $abweichungsgrundBemerkung5,
         /** @var string[] */
         readonly array $abschlagsrechnungen = [],
     ) {
