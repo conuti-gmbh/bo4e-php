4ae163ff19976ba7f3401246497abf39e5a8085b
Jenkins Auto Sync Pipeline <jenkins@conuti.de>
2024-02-26 12:36:25 +0000

    Auto Sync at Mon Feb 26 12:36:19 UTC 2024

--- src/v1/COM/AusgerollteZaehlzeit.php
+++ src/v1/COM/AusgerollteZaehlzeit.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace Conuti\BO4E\v1\COM;
 
-use DateTime;
+use Conuti\BO4E\v1\Enum\HaeufigkeitZaehlzeit;
 
 class AusgerollteZaehlzeit
 {
     public function __construct(
-        readonly ?string $aenderungszeitpunkt,
-        readonly ?string $register
+        readonly ?string $aenderungszeitpunkt = null,
+        readonly ?string $register = null,
+        readonly ?HaeufigkeitZaehlzeit $haeufigkeit = null
     ) {
     }
 }
